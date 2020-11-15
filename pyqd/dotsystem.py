a3b233ae5c9db4943d1db6f0af0fc0abf16f2316
cprosko <cprosko@gmail.com>
2020-11-15 15:39:20 +0100

    Added state mapping, other changes

--- pyqd/dotsystem.py
+++ pyqd/dotsystem.py
@@ -1,23 +1,28 @@
 """
 dotsystem:
-Module for simulating quantum dot systems with or without leads,
-including only degeneracy and charge degrees of freedom, no fermionic
-statistics.
+Module containing the main DotSystem class which manages simulations of simple
+charge-basis multi-dot simulations.
 """
 
 import numpy as np
-
 from .quantumdot import QuantumDot, SuperconductingIsland, QuasiLead
-from .utilities import is_iterable, binom
+from .utilities import binom
+from itertools import product
+
+_CHARGE_PARAM_NAMES = {"is_floating", "max_charge", "floating_charge"}
 
 
 class DotSystem:
     """Manager class for simulations of multiple dots."""
 
-    def __init__(self):
+    def __init__(self, is_floating=False, max_charge=None, floating_charge=None):
         self._dots = dict()
         self._couplings = dict()
-        pass
+        self._state_map = []
+        self._inverse_state_map = dict()
+        self._is_floating = is_floating
+        self._floating_charge = floating_charge
+        self._max_charge = max_charge
 
     @property
     def couplings(self):
@@ -31,14 +36,55 @@ class DotSystem:
     def numdots(self):
         return len(self._dots.keys())
 
-    def set_coupling(self, dot1name, dot2name, amplitude):
-        self._couplings[(dot1name, dot2name)] = amplitude
-        self._couplings[(dot2name, dot1name)] = np.conjugate(amplitude)
+    @property
+    def num_degenerate_dots(self):
+        return len([v for v in self._dots.values() if v.spin_degenerate is True])
 
-    def initialize_coupling(self, dotname, amplitude=0):
+    @property
+    def is_floating(self):
+        return self._is_floating
+
+    @is_floating.setter
+    def is_floating(self, val):
+        self._is_floating = val
+
+    def num_dots_type(self, dot_type):
+        return len([v for v in self._dots.values() if v.dot_type == dot_type])
+
+    def set_charge_params(self, **kwargs):
+        if any([k not in _CHARGE_PARAM_NAMES for k in kwargs.keys()]):
+            raise Exception(
+                "Unrecognized charge parameter input. "
+                + "Recognized keys include: {keys}".format(_CHARGE_PARAM_NAMES)
+            )
+        if "max_charge" in kwargs.keys():
+            # If is floating, max charge times number of dots must be more than floating charge
+            self._max_charge = kwargs["max_charge"]
+        if "floating_charge" in kwargs.keys():
+            self._floating_charge = kwargs["floating_charge"]
+        if "is_floating" in kwargs.keys():
+            if self._floating_charge is None or self._floating_charge <= 0:
+                raise Exception(
+                    "floating_charge must be specified if system is made floating!"
+                )
+            self._is_floating = kwargs["is_floating"]
+
+    def set_coupling(self, dot1name, dot2name, amplitude=0, amplitude2e=0, Em=0):
+        self._couplings[(dot1name, dot2name)] = {
+            "1e": amplitude,
+            "2e": amplitude2e,
+            "Em": Em,
+        }
+        self._couplings[(dot2name, dot1name)] = {
+            "1e": np.conjugate(amplitude),
+            "2e": np.conjugate(amplitude2e),
+            "Em": Em,
+        }
+
+    def initialize_coupling(self, dotname):
         existing_dot_names = [k for k in self._dots.keys() if k != dotname]
         for edn in existing_dot_names:
-            self.set_coupling(dotname, edn, amplitude)
+            self.set_coupling(dotname, edn)
 
     def remove_coupling(self, dot1name, dot2name):
         self.set_coupling(dot1name, dot2name, 0)
@@ -50,12 +96,16 @@ class DotSystem:
             del self._couplings[(rd, dotname)]
 
     def attach_dot(self, dot):
-        if dot.name in self._dots.keys():
+        if dot.name is None:
+            name = dot.dot_type + str(self.num_dots_type(dot.dot_type) + 1)
+        elif dot.name in self._dots.keys():
             raise Exception(
                 "Dot with name {n} already present in DotSystem!".format(dot.name)
             )
-        self._dots[dot.name] = dot
-        existing_dot_names = [k for k in self._dots.keys() if k != dot.name]
+        else:
+            name = dot.name
+        self._dots[name] = dot
+        existing_dot_names = [k for k in self._dots.keys() if k != name]
         for edn in existing_dot_names:
             self.set_coupling(dot.name, edn, 0)
 
@@ -63,13 +113,25 @@ class DotSystem:
         if len(args) == 1:
             dot = args[0]
             if not isinstance(dot, QuantumDot):
-                raise Exception(("Invalid object type passed to " "DotSystem.add_dot!"))
+                raise Exception(("Invalid object type passed to DotSystem.add_dot!"))
         else:
             dot = QuantumDot(*args, **kwargs)
         self.attach_dot(dot)
 
+    def add_lead(self, name=None):
+        self.add_dot(QuasiLead(name=name))
+
+    def add_island(self, *args, **kwargs):
+        self.add_dot(SuperconductingIsland(*args, **kwargs))
+
     @staticmethod
-    def num_states(max_charge, numdots, is_floating=False, floating_charge=None):
+    def num_states(
+        max_charge,
+        numdots,
+        is_floating=False,
+        floating_charge=None,
+        num_degenerate_dots=0,
+    ):
         """Calculate dimension of Hilbert space for given charge boundaries.
 
         Assumes each island/dot is allowed to have a minimum charge of 0 electrons.
@@ -91,21 +153,37 @@ class DotSystem:
         --------
         int: Dimension of the charge-state Hilbert space.
         """
+        spin_degeneracy_multiplier = 2 ** num_degenerate_dots
         if is_floating:
             # Formula from Lemma 1.1 of doi:10.2298/AADM0802222R (Joel Ratsaby) for
             # number of ordered partitions of the integer floating_charge into numdots
             # partitions of maximum size max_charge.
-            return sum(
+            return spin_degeneracy_multiplier * sum(
                 [
-                    (-1) ** (i / (max_charge + 1))
+                    (-1) ** (i // (max_charge + 1))
                     * binom(numdots, i / (max_charge + 1))
                     * binom(floating_charge - i + numdots - 1, floating_charge - i)
                     for i in np.arange(0, floating_charge + 1, max_charge + 1)
                 ]
             )
-        return np.sum(
+        return spin_degeneracy_multiplier * np.sum(
             [binom(total_charge - 1, numdots - 1) for total_charge in range(max_charge)]
         )
+
+    def _refresh_state_map(self):
+        # First update state mapping indices to charge states
+        single_dot_charge_states = [list(range(self._max_charge))] * self.numdots
+        unfixed_charge_states = np.array(list(product(*single_dot_charge_states)))
+        if not self.is_floating:
+            self._state_map = unfixed_charge_states
+        else:
+            fixed_charge_states = unfixed_charge_states[
+                np.sum(unfixed_charge_states, axis=1) == self._floating_charge
+            ]
+            self._state_map = fixed_charge_states
+        # Next, update inverse state map accordingly (mapping charge states to indices)
+        num_states = self._state_map.shape[0]
+        self._inverse_state_map = {self._state_map[i]: i for i in range(num_states)}
 
 
 def main():
--- pyqd/quantumdot.py
+++ pyqd/quantumdot.py
@@ -1,27 +1,39 @@
 """Classes containing information about quantum dots and other islands."""
 
+
 class QuantumDot:
     """Class containing properties and couplings for a single quantum dot."""
-    def __init__(self, name, chargingEnergy, levelSpacing,
-                 spinDegenerate=False, __dotType="dot"):
+
+    def __init__(
+        self,
+        charging_energy,
+        level_spacing,
+        name=None,
+        spin_degenerate=False,
+        _dot_type="dot",
+    ):
         self.name = name
-        self.chargingEnergy = chargingEnergy
-        self.levelSpacing = levelSpacing
-        self.spinDegenerate = spinDegenerate
-        self.dotType = __dotType
-    pass
+        self.charging_energy = charging_energy
+        self.level_spacing = level_spacing
+        self.spin_degenerate = spin_degenerate
+        self.dot_type = _dot_type
 
 
 class SuperconductingIsland(QuantumDot):
     """Class containing properties of a superconducting charge island."""
-    def __init__(self, name, chargingEnergy, gapSize):
-        super().__init__(name, chargingEnergy, gapSize,
-                         spinDegenerate=True, __dotType="superconducting")
-    pass
+
+    def __init__(self, charging_energy, gap_size, name=None):
+        super().__init__(
+            name,
+            charging_energy,
+            gap_size,
+            spin_degenerate=True,
+            _dot_type="superconducting",
+        )
 
 
 class QuasiLead(QuantumDot):
     """Class for naively modeling leads as 0-charging-energy dots."""
-    def __init__(self):
-        super().__init__("lead", 0, 0, spinDegenerate=False, __dotType="lead")
-    pass
+
+    def __init__(self, name=None):
+        super().__init__(0, 0, name=name, spin_degenerate=False, _dot_type="lead")
