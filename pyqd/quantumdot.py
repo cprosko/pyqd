02d5749be9f8134d2ecb20823a3a4ce4e909de57
cprosko <cprosko@gmail.com>
2023-02-13 22:09:55 +0100

    trimmed most code for overhaul

--- pyqd/quantumdot.py
+++ pyqd/quantumdot.py
@@ -9,25 +9,25 @@ class QuantumDot:
         charging_energy,
         level_spacing,
         name=None,
-        spin_degenerate=False,
+        degeneracy=1,
         _dot_type="dot",
     ):
         self.name = name
         self.charging_energy = charging_energy
         self.level_spacing = level_spacing
-        self.spin_degenerate = spin_degenerate
+        self.degeneracy = degeneracy
         self.dot_type = _dot_type
 
 
 class SuperconductingIsland(QuantumDot):
     """Class containing properties of a superconducting charge island."""
 
-    def __init__(self, charging_energy, gap_size, name=None):
+    def __init__(self, charging_energy, gap_size, name=None, degeneracy=1):
         super().__init__(
             charging_energy,
             gap_size,
             name=name,
-            spin_degenerate=True,
+            degeneracy=degeneracy,
             _dot_type="superconducting",
         )
 
@@ -36,4 +36,4 @@ class QuasiLead(QuantumDot):
     """Class for naively modeling leads as 0-charging-energy dots."""
 
     def __init__(self, name=None):
-        super().__init__(0, 0, name=name, spin_degenerate=False, _dot_type="lead")
+        super().__init__(0, 0, name=name, degeneracy=1, _dot_type="lead")
