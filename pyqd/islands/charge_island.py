156fe8978b1693286efa9893cc889e7085f0a4b4
cprosko <cprosko@gmail.com>
2023-09-28 22:48:34 +0200

    added gap as ChargeIsland param

--- pyqd/islands/charge_island.py
+++ pyqd/islands/charge_island.py
@@ -1,9 +1,14 @@
-class ChargeIsland():
+class ChargeIsland:
     """Base class for a charge island or 'quasilead'."""
-    def __init__(self, name: str, ec: float, is_sc: bool=False):
+
+    def __init__(self, name: str, ec: float, is_sc: bool = False, gap: float = None):
         self.name = name
         self.ec = ec
         self.is_sc = is_sc
+        if is_sc:
+            if gap is None:
+                raise Exception("'gap' must be specified when is_sc is True!")
+        self.gap = gap
 
     @property
     def charging_energy(self):
